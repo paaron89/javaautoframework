c87cfee324110a3f33ee3555ef1ade0d3e068221
Aron_Preszter <aron.preszter@hu.verizon.com>
2020-06-05 16:53:47 +0200

    moved json payload to separated file extracted response

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,33 +1,20 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import payloads.Payloads;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Basics {
     public static void main(String[] args) {
 
-
-
-        String jsonPayload = "{\n" +
-                "  \"location\": {\n" +
-                "    \"lat\": -38.383494,\n" +
-                "    \"lng\": 33.427362\n" +
-                "  },\n" +
-                "  \"accuracy\": 50,\n" +
-                "  \"name\": \"Rahul Shetty Academy\",\n" +
-                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                "  \"address\": \"29, side layout, cohen 09\",\n" +
-                "  \"types\": [\n" +
-                "    \"shoe park\",\n" +
-                "    \"shop\"\n" +
-                "  ],\n" +
-                "  \"website\": \"http://rahulshettyacademy.com\",\n" +
-                "  \"language\": \"French-IN\"\n" +
-                "}\n";
-
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body(jsonPayload)
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(Payloads.addPlace())
         .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().response().asString();
+        JsonPath jsonPath = new JsonPath(response);
+        String placeId = jsonPath.getString("place_id");
+        System.out.println("Place id in response is: " + placeId);
     }
 }
