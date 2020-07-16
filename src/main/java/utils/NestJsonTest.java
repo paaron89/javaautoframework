c71554842cc8698d1a49130c4ce1b2ebcd077330
Aron_Preszter <aron.preszter@hu.verizon.com>
2020-07-16 15:42:53 +0200

    added new print

--- src/main/java/utils/NestJsonTest.java
+++ src/main/java/utils/NestJsonTest.java
@@ -16,19 +16,28 @@ public class NestJsonTest {
         Integer sizeOfCourses = coursesJson.getInt("courses.size()");
         System.out.println(sizeOfCourses);
 
-        for(int i = 0;  i < sizeOfCourses;  i ++ ){
+        for (int i = 0; i < sizeOfCourses; i++) {
             String title = coursesJson.getString("courses[ " + i + "].title");
             System.out.println(title);
-            if(title.equalsIgnoreCase("rpa")){
+            if (title.equalsIgnoreCase("rpa")) {
                 int price = coursesJson.getInt("courses[ " + i + "].price");
                 System.out.println("Course called: " + title + " costs: " + price);
 
             }
 
         }
+
+        System.out.println("Print number of copies if title RPA");
+        for (int i = 0; i < sizeOfCourses; i++) {
+            if (coursesJson.getString("courses[ " + i + "].title").equalsIgnoreCase("RPA")) {
+                System.out.println(coursesJson.get("courses[ " + i + "].copies"));
+                break;
+            }
+        }
+
+
         String firstCourse = coursesJson.getString("courses[0].title");
         System.out.println("The first course is: " + firstCourse);
-
 
 
     }
