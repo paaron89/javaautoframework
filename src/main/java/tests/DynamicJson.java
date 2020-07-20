9c59f917203ad98e29f44b3d31d2cbcc732858b0
Aron_Preszter <aron.preszter@hu.verizon.com>
2020-07-20 10:47:57 +0200

    Added a delete testcase, added parametrization

---
+++ src/main/java/tests/DynamicJson.java
@@ -0,0 +1,42 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import payloads.Payloads;
+import utils.Helpers;
+
+public class DynamicJson {
+
+    private String baseUrl = "http://216.10.245.166";
+
+    @Test(dataProvider="dataprovider")
+    public void addBook(String isbn, String aisle){
+        RestAssured.baseURI = baseUrl;
+        String responseAddBokk = RestAssured.given().log().all().header("Content-Type","application/json")
+                .body(Payloads.addBook(isbn, aisle))
+                .when().post("/Library/Addbook.php")
+                .then().assertThat().statusCode(200).extract().response().asString();
+
+        System.out.println(responseAddBokk);
+        String bookId = Helpers.rawToJson(responseAddBokk).get("ID");
+        System.out.println("Your book id is: " + bookId);
+
+    }
+
+    @Test(dataProvider = "dataprovider")
+    public void deleteBook(String isbn, String aisle){
+        RestAssured.baseURI = baseUrl;
+        String rDeleteBook = RestAssured.given().log().all().header("Content-Type", "application/json")
+                .body("{\"ID\":isbn+aisle}")
+                .when().post("/Library/DeleteBook.php")
+                .then().assertThat().statusCode(200).extract().response().asString();
+    }
+
+
+
+    @DataProvider(name = "dataprovider")
+    public Object [][] getdata(){
+        return new Object[][] {{"testisbn", "testaisle"}, {"testdata1", "testdata2"}, {"valami1", "valami2"}};
+    }
+}
