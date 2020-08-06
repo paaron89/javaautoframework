c0f7c535d05f57c3f98de39c689f5713eb10ea34
Aaron <presztaron89@gmail.com>
2020-08-06 09:43:10 +0200

    start of oauth2

---
+++ src/main/java/tests/oAuthTest.java
@@ -0,0 +1,13 @@
+package tests;
+import static io.restassured.RestAssured.given;
+
+public class oAuthTest {
+    public static void main(String[] args) {
+
+
+        String response = given().queryParam("access_token", "")
+                .when().get("https://rahulshettyacademy.com/getCourse.php").asString();
+        System.out.println("My response: " + response);
+
+    }
+}
